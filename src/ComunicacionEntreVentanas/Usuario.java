030cdbd4750b11751c0937c53ace830a2a8909c4
DAM2Alu5 <DAM2Alu5@B01-PC10.IESGUARA.LOCAL>
2023-10-04 10:21:31 +0200

    si si si

---
+++ src/ComunicacionEntreVentanas/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ComunicacionEntreVentanas;
+public class Usuario {
+    
+    
+    String nombre, apellido;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+
+  
+   
+    
+    
+    
+    
+    
+}
